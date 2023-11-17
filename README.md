2e5ecb81302adb111b190c35a802ed2d0da7a25e
Renzxs <florencebatol85@gmail.com>
2023-11-17 20:17:40 +0800

    Added
    Font Awesome API usability

--- README.md
+++ README.md
@@ -17,6 +17,15 @@ FILE DIRECTORIES:
 - ../scripts/ - Javascripts Directory
 - ../styles/ - CSS Directory
 
+FOR ICONS:
+```
+<!-- PUT THIS WITHIN HEAD TAG OF YOUR HTML FILE -->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<!-- TO USE THE ICON IN THE HTML FILE -->
+<i class="fa-solid fa-house"></i> <!--THIS WILL SHOW A SOLID ICON OF A HOUSE -->
+```
 
 
 - - - -
--- admin-side/users.php
+++ admin-side/users.php
@@ -6,8 +6,16 @@
     <title>SupetPet | Manage Users</title>
     <link rel="stylesheet" href="users.css">
     <link rel="icon" href="../assets/images/admin_logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    
+    <div class="container">
+        <div class="nav-sidebar">
+            
+        </div>
+        <div class="main-container">
+
+        </div>
+    </div>
 </body>
 </html>
