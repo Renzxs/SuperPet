85c766a90eb3ffadd889a3bb0ee6beeb45e2ac7c
Renzxs <florencebatol85@gmail.com>
2023-11-22 21:04:44 +0800

    Added Home Page

---
+++ homepage/home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperPet | Home</title>
+    <link rel="icon" href="../assets/images/admin_logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>  
+    <div class="container">
+        <div class="nav-bar">
+            <i class="fa-solid fa-bars" id="menu-nav-btn"></i>
+            <img src="../assets/images/Letter_Logo.png" alt="" width="150px">
+            <nav>
+                <ul class="nav-links">
+                    <li class="in"><a href="#">Home</a></li>
+                    <li><a href="#">Book an Appointment</a></li>
+                    <li><a href="#">SuperPet Products</a></li>
+                    <li><a href="#">Adopt your SuperPet</a></li>
+                </ul>
+            </nav>
+            <div class="nav-icon-btns">
+                <i class="fa-solid fa-shopping-cart"></i>
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
+        </div>
+        <div class="announcement">
+            <p>Christmas Sale   •  Get 50% OFF for your first order! </p>
+        </div>
+        <div class="banner">
+            <img src="../assets/images/HERO-BG.png" alt="" width="200">
+            <div class="banner-txt">
+                <img src="../assets/images/banner-text.png" alt="" width="100">
+                <p class="sub-desc"><strong>Discover SuperPet, where adorable meets superhero.</strong> Elevate your pet's well-being with our whimsical, expert care!</p>
+                <a href="#" class="appointment-btn">BOOK AN APPOINTMENT</a>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
