a1ca7a44db2b491aa72fb2898d3791b0da69d882
Renzxs <florencebatol85@gmail.com>
2023-11-22 13:25:05 +0800

    Fixed Path

--- admin-side/admin-login.php
+++ admin-side/admin-login.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SuperPet | Admin Login</title>
-        <link rel="stylesheet" href="admin-login.css">
+        <link rel="stylesheet" href="styles/admin-login.css">
         <link rel="icon" href="../assets/images/superpet_logo.png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
--- admin-side/users.php
+++ admin-side/users.php
@@ -23,10 +23,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SupetPet | Manage Users</title>
-    <link rel="stylesheet" href="users.css">
+    <link rel="stylesheet" href="styles/users.css">
     <link rel="icon" href="../assets/images/admin_logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="App.js" defer></script>
+    <script src="scripts/App.js" defer></script>
 </head>
 <body>
     <div class="container">
