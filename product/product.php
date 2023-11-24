09a04c6bccc2c2f4e35258bf77b320171b3fe4ac
Renzxs <florencebatol85@gmail.com>
2023-11-24 19:01:55 +0800

    Mobile Nav Added

--- product/product.php
+++ product/product.php
@@ -7,6 +7,7 @@
     <link rel="icon" href="../assets/images/admin_logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="product.css">
+    <script src="Product.js" defer></script>
 </head>
 <body>  
     <div class="container">
@@ -25,6 +26,17 @@
                 <i class="fa-solid fa-shopping-cart"></i>
                 <i class="fa-solid fa-circle-user"></i>
             </div>
+        </div>
+
+        <div class="mobile-nav">
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="../homepage/home.php">Home</a></li>
+                    <li><a href="#">Book an Appointment</a></li>
+                    <li class="in"><a href="../product/product.php">SuperPet Products</a></li>
+                    <li><a href="#">Adopt your SuperPet</a></li>
+                </ul>
+            </nav>
         </div>
 
         <div class="product-page">
