7a2a627ad64e9846db89fc87403242fa06f7918d
thei <hanni.bare@gmail.com>
2023-11-25 23:01:29 +0800

    Adopt Page Added

---
+++ adoptpage/adoptpage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperPet | Adopt</title>
+    <link rel="icon" href="../assets/images/admin_logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="adopt.css">
+    <script src="Product.js" defer></script>
+</head>
+<body>  
+    <div class="container">
+        <div class="nav-bar">
+            <i class="fa-solid fa-bars" id="menu-nav-btn"></i>
+            <img src="../assets/images/Letter_Logo.png" alt="" width="150px">
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="../homepage/home.php">Home</a></li>
+                    <li><a href="../appointmentpage/appointment.php">Book an Appointment</a></li>
+                    <li><a href="../product/product.php">SuperPet Products</a></li>
+                    <li class="in"><a href="#">Adopt your SuperPet</a></li>
+                </ul>
+            </nav>
+            <div class="nav-icon-btns">
+                <i class="fa-solid fa-shopping-cart"></i>
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
+        </div>
+
+        <div class="mobile-nav">
+            <nav>
+                <ul class="nav-links">
+                    <li ><a href="#">Home</a></li>
+                    <li><a href="../appointmentpage/appointment.php">Book an Appointment</a></li>
+                    <li class="in"><a href="../product/product.php">SuperPet Products</a></li>
+                    <li><a href="#">Adopt your SuperPet</a></li>
+                </ul>
+            </nav>
+        </div>
